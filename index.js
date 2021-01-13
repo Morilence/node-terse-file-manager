9508e2b4226a4a6524131bcd9f64dfef0833e088
Morilence <1280659615@qq.com>
2021-01-13 22:24:21 +0800

    first commit.

---
+++ index.js
@@ -0,0 +1,378 @@
+const fs = require("fs");
+const path = require("path");
+
+class FileManagerError extends Error {
+    constructor(msg) {
+        super(msg);
+        this.name = "FileManagerError";
+    }
+}
+
+module.exports = class FileManager {
+    constructor(p) {
+        this.root = path.resolve(p);
+    }
+
+    /**
+     * @param {string|undefined} p Path of dir. (optional)
+     * @description Browse from the root when p is undefined.
+     */
+    async browse(p) {
+        let view = null;
+        if (p == undefined) {
+            view = await this._scandir(this.root);
+        } else {
+            const ap = this._pretreat(p);
+            const { ext } = path.parse(ap);
+            if (ext == "") {
+                if (await this._access(ap, fs.constants.F_OK)) {
+                    view = await this._scandir(ap);
+                } else {
+                    throw new FileManagerError("Target doesn't exist.");
+                }
+            } else {
+                throw new FileManagerError("Target must be a folder.");
+            }
+        }
+        return view;
+    }
+
+    /**
+     * @param {array} list An array of objects. Each object has p and buf prop to appoint path and data(Buffer).
+     * @param {boolean} cover Whether to overwrite the target with the same name.
+     */
+    async create(list, cover = false) {
+        for (const item of list) {
+            const ap = this._pretreat(item.p);
+            const { ext } = path.parse(ap);
+            if (ext == "") {
+                await this._newdir(ap);
+            } else {
+                await this._newfile(ap, item.buf == undefined ? Buffer.from("") : item.buf, cover);
+            }
+        }
+        return { res: "ok" };
+    }
+
+    /**
+     * @param {array} list An array of objects. Each object has sp and dp prop to appoint srcpath and destpath.
+     * @param {boolean} cover Whether to overwrite the target with the same name.
+     */
+    async copy(list, cover = true) {
+        for (const item of list) {
+            const asp = this._pretreat(item.sp);
+            const adp = this._pretreat(item.dp);
+            const opext = path.parse(asp).ext;
+            const npext = path.parse(adp).ext;
+            if (opext == npext && opext == "") {
+                await this._copydir(asp, adp, cover);
+            } else if (opext == npext && opext != "") {
+                await this._copyfile(asp, adp, cover);
+            } else {
+                throw new FileManagerError("Source and target types do not match.");
+            }
+        }
+        return { res: "ok" };
+    }
+
+    /**
+     * @param {array} list An array of objects. Each object has sp and dp prop to appoint srcpath and destpath.
+     * @param {boolean} cover Whether to overwrite the target with the same name.
+     */
+    async cut(list, cover = true) {
+        for (const item of list) {
+            const asp = this._pretreat(item.sp);
+            const adp = this._pretreat(item.dp);
+            const opext = path.parse(asp).ext;
+            const npext = path.parse(adp).ext;
+            if (opext == npext && opext == "") {
+                await this._copydir(asp, adp, cover);
+                await this._removedir(asp);
+            } else if (opext == npext && opext != "") {
+                await this._copyfile(asp, adp, cover);
+                await this._removefile(asp);
+            } else {
+                throw new FileManagerError("Source and target types do not match.");
+            }
+        }
+        return { res: "ok" };
+    }
+
+    /**
+     * @param {array} list An array of objects. Each object has op and np prop to appoint oldpath and newpath.
+     */
+    async rename(list) {
+        for (const item of list) {
+            const aop = this._pretreat(item.op);
+            const anp = this._pretreat(item.np);
+            if (path.relative(path.dirname(aop), path.dirname(anp)) != "") {
+                throw new FileManagerError("Directory mismatch.");
+            }
+            await this._rename(aop, anp);
+        }
+        return { res: "ok" };
+    }
+
+    /**
+     * @param {array} list Path array.
+     */
+    async remove(list) {
+        for (const p of list) {
+            const ap = this._pretreat(p);
+            const { ext } = path.parse(ap);
+            if (ext == "") {
+                await this._removedir(ap);
+            } else {
+                await this._removefile(ap);
+            }
+        }
+        return { res: "ok" };
+    }
+
+    /**
+     * @desciption Clear all items in the folder bound to the current instance.
+     */
+    async clear() {
+        for await (const dirent of await fs.promises.opendir(this.root)) {
+            if (dirent.isFile()) {
+                await this._removefile(path.resolve(this.root, dirent.name));
+            } else if (dirent.isDirectory()) {
+                await this._removedir(path.resolve(this.root, dirent.name));
+            } else {
+                throw new FileManagerError("Unrecognized target.");
+            }
+        }
+        return { res: "ok" };
+    }
+
+    /**
+     * @param {array} tasks An array of objects. Each object represent a task which use nm prop to appoint operation.
+     */
+    async bulk(tasks) {
+        for (const task of tasks) {
+            switch (task.nm) {
+                case "create":
+                    await this.create(task.ls, task.cvr == undefined ? true : task.cvr);
+                    break;
+                case "copy":
+                    await this.copy(task.ls, task.cvr == undefined ? true : task.cvr);
+                    break;
+                case "cut":
+                    await this.cut(task.ls, task.cvr == undefined ? true : task.cvr);
+                    break;
+                case "rename":
+                    await this.rename(task.ls);
+                    break;
+                case "remove":
+                    await this.remove(task.ls);
+                    break;
+                case "clear":
+                    await this.clear();
+                    break;
+                default:
+                    throw new FileManagerError("Unknown operation.");
+            }
+        }
+        return { res: "ok" };
+    }
+
+    _pretreat(p) {
+        const _this = this;
+
+        function __toAP(p) {
+            return path.resolve(_this.root, p);
+        }
+
+        function __isLegal(ap) {
+            return !(
+                path.relative(_this.root, ap).length == 0 ||
+                (path.relative(_this.root, ap).indexOf("..") == 0 && _this.root.length > ap.length)
+            );
+        }
+
+        const ap = __toAP(p);
+        if (!__isLegal(ap)) throw new FileManagerError("Illegal path.");
+        return ap;
+    }
+
+    _scandir(ap) {
+        const res = [];
+        return new Promise((resolve, reject) => {
+            (async () => {
+                try {
+                    for await (const dirent of await fs.promises.opendir(ap)) {
+                        if (dirent.isFile()) {
+                            res.push({
+                                name: dirent.name,
+                                isdir: false,
+                            });
+                        } else if (dirent.isDirectory()) {
+                            res.push({
+                                name: dirent.name,
+                                isdir: true,
+                                children: await this._scandir(path.resolve(ap, dirent.name)),
+                            });
+                        } else {
+                            throw new FileManagerError("Unrecognized target.");
+                        }
+                    }
+                    resolve(res);
+                } catch (err) {
+                    reject(err);
+                }
+            })();
+        });
+    }
+
+    _newfile(ap, buf, cover) {
+        return new Promise((resolve, reject) => {
+            (async () => {
+                try {
+                    await this._newdir(path.dirname(ap));
+                    if (
+                        !(await this._access(ap, fs.constants.F_OK)) ||
+                        ((await this._access(ap, fs.constants.F_OK)) && cover)
+                    ) {
+                        const wstream = fs.createWriteStream(ap);
+                        wstream.on("open", () => {
+                            const blockSize = 128;
+                            const numOfBlocks = Math.ceil(buf.length / blockSize);
+                            for (let i = 0; i < numOfBlocks; i++) {
+                                const currBlock = buf.slice(blockSize * i, Math.min(blockSize * (i + 1), buf.length));
+                                wstream.write(currBlock);
+                            }
+                            wstream.end();
+                        });
+                        wstream.on("error", err => {
+                            throw err;
+                        });
+                        wstream.on("finish", () => {
+                            resolve(true);
+                        });
+                    } else {
+                        throw new FileManagerError("Target already exists.");
+                    }
+                } catch (err) {
+                    reject(err);
+                }
+            })();
+        });
+    }
+
+    _newdir(ap) {
+        return new Promise((resolve, reject) => {
+            fs.mkdir(ap, { recursive: true }, err => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(true);
+                }
+            });
+        });
+    }
+
+    _copyfile(asp, adp, cover) {
+        return new Promise((resolve, reject) => {
+            (async () => {
+                try {
+                    await this._newdir(path.dirname(adp));
+                    if (
+                        !(await this._access(adp, fs.constants.F_OK)) ||
+                        ((await this._access(adp, fs.constants.F_OK)) && cover)
+                    ) {
+                        const rstream = fs.createReadStream(asp);
+                        const wstream = fs.createWriteStream(adp);
+                        rstream.pipe(wstream);
+                    } else {
+                        throw new FileManagerError("Target already exists.");
+                    }
+                    resolve(true);
+                } catch (err) {
+                    reject(err);
+                }
+            })();
+        });
+    }
+
+    _copydir(asp, adp, cover) {
+        return new Promise((resolve, reject) => {
+            (async () => {
+                try {
+                    await this._newdir(adp);
+                    for await (const dirent of await fs.promises.opendir(asp)) {
+                        if (dirent.isFile()) {
+                            await this._copyfile(path.resolve(asp, dirent.name), path.resolve(adp, dirent.name), cover);
+                        } else if (dirent.isDirectory()) {
+                            await this._copydir(path.resolve(asp, dirent.name), path.resolve(adp, dirent.name), cover);
+                        } else {
+                            throw new FileManagerError("Unrecognized target.");
+                        }
+                    }
+                    resolve(true);
+                } catch (err) {
+                    reject(err);
+                }
+            })();
+        });
+    }
+
+    _rename(aop, anp) {
+        return new Promise((resolve, reject) => {
+            fs.rename(aop, anp, err => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(true);
+                }
+            });
+        });
+    }
+
+    _removefile(ap) {
+        return new Promise((resolve, reject) => {
+            fs.unlink(ap, err => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(true);
+                }
+            });
+        });
+    }
+
+    _removedir(ap) {
+        return new Promise((resolve, reject) => {
+            fs.rmdir(ap, { recursive: true }, err => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(true);
+                }
+            });
+        });
+    }
+
+    _access(ap, mode) {
+        return new Promise(resolve => {
+            fs.access(ap, mode, err => {
+                if (err) {
+                    resolve(false);
+                } else {
+                    resolve(true);
+                }
+            });
+        });
+    }
+
+    _stat(ap) {
+        return new Promise((resolve, reject) => {
+            fs.stat(ap, (err, stats) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(stats);
+                }
+            });
+        });
+    }
+};
---
+++ test/index.js
@@ -0,0 +1,101 @@
+const path = require("path");
+const FileManager = require("../index");
+
+// select a folder as the root directory for management
+const fm = new FileManager(path.resolve(__dirname));
+
+// demonstration
+(async () => {
+    // create
+    await fm
+        .create(
+            [
+                { p: "./temp" },
+                { p: "./temp/a" },
+                { p: "./temp/b" },
+                { p: "./temp/c" },
+                { p: "./temp/a/aa" },
+                { p: "./temp/a/ab" },
+                { p: "./temp/a/ac" },
+                { p: "./temp/a/aa/hello.txt", buf: Buffer.from("Hello, world!") },
+            ],
+            true
+        )
+        .then(res => {
+            console.log("create:", res);
+        });
+
+    // copy
+    await fm.copy([{ sp: "./temp", dp: "./temp3" }], true).then(res => {
+        console.log("copy:", res);
+    });
+
+    // cut
+    await fm.cut([{ sp: "./temp3", dp: "./temp2" }], true).then(res => {
+        console.log("cut:", res);
+    });
+
+    // remove
+    await fm.remove(["./temp"]).then(res => {
+        console.log("remove:", res);
+    });
+
+    // rename
+    await fm.rename([{ op: "./temp2", np: "./tmp" }]).then(res => {
+        console.log("rename:", res);
+    });
+
+    // bulk
+    await fm
+        .bulk([
+            {
+                nm: "create",
+                ls: [
+                    { p: "./tmp/d" },
+                    { p: "./tmp/d/da" },
+                    { p: "./tmp/d/db" },
+                    { p: "./tmp/d/dc" },
+                    { p: "./tmp/d/db/index.html", buf: Buffer.from("") },
+                ],
+                cvr: true, // default
+            },
+            {
+                nm: "cut",
+                ls: [{ sp: "./tmp/d/db/index.html", dp: "./tmp/d/da/index.html" }],
+            },
+            {
+                nm: "copy",
+                ls: [
+                    { sp: "./tmp/d/da/index.html", dp: "./tmp/d/da/index2.html" },
+                    { sp: "./tmp/d/da/index.html", dp: "./tmp/d/da/index3.html" },
+                ],
+            },
+            {
+                nm: "rename",
+                ls: [
+                    { op: "./tmp/d/da/index2.html", np: "./tmp/d/da/index.css" },
+                    { op: "./tmp/d/da/index3.html", np: "./tmp/d/da/index.js" },
+                ],
+            },
+        ])
+        .then(res => {
+            console.log("bulk:", res);
+        });
+
+    // browse
+    function dfsprint(tree, prefix = "") {
+        for (const node of tree) {
+            process.stdout.write(prefix);
+            if (!node.isdir) {
+                console.log(node.name);
+            } else {
+                console.log(node.name + "/");
+                dfsprint(node.children, prefix + "-");
+            }
+        }
+    }
+    dfsprint(await fm.browse());
+
+    // clear
+    await fm.clear();
+})();
